0c95381d8b750439437662a84563c5d6bec7522e
KhalidAlouan <43475316+KhalidAlouan@users.noreply.github.com>
2019-06-01 11:51:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -20,25 +20,25 @@
  //echo "GET: ".var_dump($_GET);
  if(isset( $_GET['delete'] )){
     $task = $_GET['delete'];
-    $query = $db->prepare("DELETE FROM mytasks WHERE id = ".$task.";");
+    $query = $db->prepare("DELETE FROM mytask WHERE id = ".$task.";");
     $query->execute();
  }
  if(isset($_GET['PorHacer'])){
-  $query = $db->prepare("UPDATE mytasks SET hecho = 0 WHERE id = ".$_GET['PorHacer'].";");
+  $query = $db->prepare("UPDATE mytask SET estado = 0 WHERE id = ".$_GET['PorHacer'].";");
   $query->execute();
  }
  if(isset($_GET['hecho'])){
-  $query = $db->prepare("UPDATE mytasks SET hecho = 1 WHERE id = ".$_GET['hecho'].";");
+  $query = $db->prepare("UPDATE mytask SET estado = 1 WHERE id = ".$_GET['hecho'].";");
   $query->execute();
 }
   if($_SERVER["REQUEST_METHOD"] == "POST"){
     $nom = $_POST['nom'];
-    $query = $db->prepare("INSERT INTO mytasks (descripcio,hecho) VALUES ('".$nom."',0)");
+    $query = $db->prepare("INSERT INTO mytask (nombre,estado) VALUES ('".$nom."',0)");
     $query->execute();
   }
    //mostrar todo
-    $result2 = $db->query("SELECT * FROM mytasks where hecho = 0");
-    $result1 = $db->query("SELECT * FROM mytasks where hecho = 1");
+    $result2 = $db->query("SELECT * FROM mytask where estado = 0");
+    $result1 = $db->query("SELECT * FROM mytask where estado = 1");
   ?>
   <body>
     <h1>TASKAS EDITYON</h1>
@@ -57,7 +57,7 @@
       echo "<table>\n";
       foreach($result1 as $row1){
         echo "\t<tr>\n";
-          echo "\t\t<td>".$row1['descripcio']." <a href='index.php?delete=".$row1['id']."'>Eliminar</a></td>\n";
+          echo "\t\t<td>".$row1['nombre']." <a href='index.php?delete=".$row1['id']."'>Eliminar</a></td>\n";
           echo "\t\t<td>Hecho <a href='index.php?PorHacer=".$row1['id']."'>Cambiar</a></td>\n";        
           echo "\t</tr>\n";
       }
@@ -67,7 +67,7 @@
       echo "<table>\n";
       foreach($result2 as $row2){
         echo "\t<tr>\n";
-          echo "\t\t<td>".$row2['descripcio']." <a href='index.php?delete=".$row2['id']."'>Eliminar</a></td>\n";
+          echo "\t\t<td>".$row2['nombre']." <a href='index.php?delete=".$row2['id']."'>Eliminar</a></td>\n";
           echo "\t\t<td>Por Hacer <a href='index.php?hecho=".$row2['id']."'>Cambiar</a></td>\n";    
           echo "\t</tr>\n";
       }
