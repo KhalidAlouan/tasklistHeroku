61f745af0940a44379b919a8e040f070603b0b24
KhalidAlouan <kalouan@iesesteveterradas.cat>
2019-06-01 14:48:56 +0200

    Tk

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html lang="es">
   <head>
     <meta charset="utf-8">
-    <title>My Task List</title>
+    <title>Task List Khalid</title>
   </head>
  <?php
-  //conexion en PDO
+ 
       $database = parse_url(getenv("DATABASE_URL"));
       $db = new PDO("pgsql:" . sprintf(
         "host=%s;port=%s;user=%s;password=%s;dbname=%s",
@@ -17,57 +17,57 @@
       ));
 
 
- //echo "GET: ".var_dump($_GET);
+
  if(isset( $_GET['delete'] )){
     $task = $_GET['delete'];
-    $query = $db->prepare("DELETE FROM mytasks WHERE id = ".$task.";");
+    $query = $db->prepare("DELETE FROM mytask WHERE id = ".$task.";");
     $query->execute();
  }
  if(isset($_GET['PorHacer'])){
-  $query = $db->prepare("UPDATE mytasks SET hecho = 0 WHERE id = ".$_GET['PorHacer'].";");
+  $query = $db->prepare("UPDATE mytask SET estado = 0 WHERE id = ".$_GET['PorHacer'].";");
   $query->execute();
  }
  if(isset($_GET['hecho'])){
-  $query = $db->prepare("UPDATE mytasks SET hecho = 1 WHERE id = ".$_GET['hecho'].";");
+  $query = $db->prepare("UPDATE mytask SET estado = 1 WHERE id = ".$_GET['hecho'].";");
   $query->execute();
 }
   if($_SERVER["REQUEST_METHOD"] == "POST"){
     $nom = $_POST['nom'];
-    $query = $db->prepare("INSERT INTO mytasks (descripcio,hecho) VALUES ('".$nom."',0)");
+    $query = $db->prepare("INSERT INTO mytask (nombre,estado) VALUES ('".$nom."',0)");
     $query->execute();
   }
-   //mostrar todo
-    $result2 = $db->query("SELECT * FROM mytasks where hecho = 0");
-    $result1 = $db->query("SELECT * FROM mytasks where hecho = 1");
+  
+    $result2 = $db->query("SELECT * FROM mytask where estado = 0");
+    $result1 = $db->query("SELECT * FROM mytask where estado = 1");
   ?>
   <body>
-    <h1>TASKAS EDITYON</h1>
+    <h1>TASK LIST</h1>
     
     <?php
     echo "
     <div>
     <form action='' method='post'>
-        Nova tasca:
+        Nueva tarea:
         <input type='text' name='nom'>
-        <input type='submit' value='nova tasca'>
+        <input type='submit' value='nueva tarea'>
     </form>";
     echo "</div>";
-    // hechos
+    // done
     echo "<h3>Hechos</h3>";
       echo "<table>\n";
       foreach($result1 as $row1){
         echo "\t<tr>\n";
-          echo "\t\t<td>".$row1['descripcio']." <a href='index.php?delete=".$row1['id']."'>Eliminar</a></td>\n";
+          echo "\t\t<td>".$row1['nombre']." <a href='index.php?delete=".$row1['id']."'>Eliminar</a></td>\n";
           echo "\t\t<td>Hecho <a href='index.php?PorHacer=".$row1['id']."'>Cambiar</a></td>\n";        
           echo "\t</tr>\n";
       }
       echo "</table>\n";
-      //por hacer
+      //not done
       echo "<h3>Por Hacer</h3>";
       echo "<table>\n";
       foreach($result2 as $row2){
         echo "\t<tr>\n";
-          echo "\t\t<td>".$row2['descripcio']." <a href='index.php?delete=".$row2['id']."'>Eliminar</a></td>\n";
+          echo "\t\t<td>".$row2['nombre']." <a href='index.php?delete=".$row2['id']."'>Eliminar</a></td>\n";
           echo "\t\t<td>Por Hacer <a href='index.php?hecho=".$row2['id']."'>Cambiar</a></td>\n";    
           echo "\t</tr>\n";
       }
